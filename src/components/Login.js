cfb5505b148cc5bdc9dd20b482d9fef2ccf702b8
Ankit Kumar Ojha <akojha498@gmail.com>
2024-11-07 11:29:35 +0530

    Added Paginstion and updated login

--- src/components/Login.js
+++ src/components/Login.js
@@ -74,8 +74,8 @@ export default function Login() {
     
     <div className="rounded-sm border shadow-default   border-strokedark   bg-boxdark">
       <div className="flex flex-wrap items-center">
-        <div className="w-full    border-strokedark xl:border-l-2">
-          <div className="w-full p-4 sm:p-12.5 xl:p-17.5">
+        <div className="w-full    border-strokedark xl:border-l-2 ">
+          <div className="w-full p-4 sm:p-12.5 xl:p-17.5 ">
             <h2 className="mb-9 text-2xl font-bold    text-white sm:text-title-xl2">
               Sign In to CRM App
             </h2>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,7 +4,7 @@ import Login from "../components/Login";
 export default function LoginPage() {
   return (
     <>
-      <div className="min-h-full h-screen flex  justify-center py-12 px-4">
+      <div className="min-h-full h-screen flex  justify-center py-12 px-4 bg-boxdark-2">
         <div className="max-w-md w-full mt-40 ">
           <Login />
         </div>
